30eb52b0c94fe91c9d594ff9b856937e8ffcecba
jadhavharshh <harshjadhavconnect@gmail.com>
2025-04-26 23:11:07 +0530

    Setup project and shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      <h1>Welcome to the Page</h1>
-      <p>This is a simple page component.</p>
+    <div className="flex flex-col h-screen w-screen justify-center items-center">
+          <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Button>Hello World</Button>
     </div>
   )
 }
